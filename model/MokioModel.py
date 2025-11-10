8a3ab9621b2e9b6ab71a8447fccef8aed3e7bb00
WOODQ <woodq41@gmail.com>
2025-11-10 09:51:10 +0800

    MoE: MoEGate

--- model/MokioModel.py
+++ model/MokioModel.py
@@ -21,6 +21,16 @@ class MokioMindConfig(PretrainedConfig):
         rope_theta: int = 1000000,
         inference_rope_scaling: bool = False,
         flash_attention: bool = True,
+        
+        ############ MoE ############
+        use_moe:bool=False,
+        num_experts_per_tok:int=2,
+        n_routed_experts:int=4,
+        n_shared_experts:int=1,
+        scoring_func:str='softmax',
+        aux_loss_alpha:float=0.1,
+        seq_aux:bool=True,
+        norm_topk_prob:bool=True,
         **kwargs,
     ):
         super().__init__(**kwargs)
@@ -40,6 +50,14 @@ class MokioMindConfig(PretrainedConfig):
         self.rope_theta = rope_theta
         self.inference_rope_scaling = inference_rope_scaling
         self.flash_attention = flash_attention
+        self.use_moe=use_moe
+        self.num_experts_per_tok=num_experts_per_tok
+        self.n_routed_experts=n_routed_experts
+        self.n_shared_experts=n_shared_experts
+        self.seq_aux=seq_aux
+        self.norm_topk_prob=norm_topk_prob
+        self.aux_loss_alpha=aux_loss_alpha
+        self.scoring_func=scoring_func
 
         self.rope_scaling = (
             {
@@ -286,6 +304,66 @@ class FeedForward(nn.Module):
     def forward(self, x):
         gated = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
         return self.dropout(self.down_proj(gated))
+    
+class MoEGate(nn.Module):
+    def __init__(self,config:MokioMindConfig):
+        super().__init__()
+        self.config=config
+        self.top_k=config.num_experts_per_tok
+        
+        self.scoring_func=config.scoring_func
+        self.alpha=config.aux_loss_alpha
+        self.seq_aux=config.seq_aux
+        
+        self.norm_topk_prob=config.norm_topk_prob
+        self.gating_dim=config.hidden_size
+        self.weight=nn.Parameter(torch.empty((self.n_routed_experts,self.gating_dim)))
+        self.reset_parameters()
+        
+    def reset_parameters(self)->None:
+        init.kaiming_uniform_(self.weight,a=math.sqrt(5))
+        
+    def forward(self,hidden_states):
+        bsz,seq_len,h=hidden_states.shape
+        hidden_states=hidden_states.view(-1,h)
+        logits=F.linear(hidden_states,self.weight,None)
+    
+        if self.scoring_func == 'softmax':
+                scores = logits.softmax(dim=-1)
+        else:
+            raise NotImplementedError(f'insupportable scoring function for MoE gating: {self.scoring_func}')
+            
+        topk_weight, topk_idx = torch.topk(scores, k=self.top_k, dim=-1, sorted=False)
+        
+        if self.top_k > 1 and self.norm_topk_prob:
+            denominator = topk_weight.sum(dim=-1, keepdim=True) + 1e-20
+            topk_weight = topk_weight / denominator
+                
+        if self.training and self.alpha > 0.0:
+            scores_for_aux = scores
+            aux_topk = self.top_k
+            topk_idx_for_aux_loss = topk_idx.view(bsz, -1)
+            if self.seq_aux:
+                scores_for_seq_aux=scores_for_aux.view(bsz,seq_len,-1)
+                ce = torch.zeros(bsz, self.n_routed_experts, device=hidden_states.device)
+                ce.scatter_add_(1, topk_idx_for_aux_loss, torch.ones(bsz, seq_len * aux_topk, device=hidden_states.device))
+                ce = ce.div(seq_len * aux_topk / self.n_routed_experts)
+                aux_loss = (ce * scores_for_seq_aux.mean(dim=1)).sum(dim=1).mean() * self.alpha
+            else:
+                mask_ce = F.one_hot(topk_idx_for_aux_loss.view(-1), num_classes=self.n_routed_experts)
+                ce = mask_ce.float().mean(0)
+                Pi = scores_for_aux.mean(0)
+                fi = ce * self.n_routed_experts
+                aux_loss = (Pi * fi).sum() * self.alpha
+        else:
+            aux_loss = 0
+        return topk_weight, topk_idx, aux_loss
+    
+
+
+        return expert_cache
+            
+        
     
 class MokioMindBlock(nn.Module):
     def __init__(self, layer_id: int, config: MokioMindConfig):
